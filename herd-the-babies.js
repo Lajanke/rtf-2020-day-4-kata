4a80181908e115e9ea441c35732aecb2bb7c2c26
Lajanke <l.j.kenny@hotmail.com>
2020-03-29 16:36:13 +0100

    Kata solution

--- herd-the-babies.js
+++ herd-the-babies.js
@@ -1,3 +1,20 @@
-const herdTheBabies = () => {};
+const herdTheBabies = (string) => {
+    const strArr = [...string];
+
+    strArr.sort((a, b) => {
+
+        const A = a.toUpperCase();
+        const B = b.toUpperCase();
+
+        if (A === B) {
+            return a < b ? -1 : 1;
+        }
+        else {
+            return A < B ? -1 : 1;
+        }
+    });
+
+    return strArr.join("");
+};
 
 module.exports = herdTheBabies;
--- spec/herd-the-babies.spec.js
+++ spec/herd-the-babies.spec.js
@@ -1,4 +1,21 @@
 const { expect } = require("chai");
 const herdTheBabies = require("../herd-the-babies");
 
-describe("heardTheBabies", () => {});
+describe("heardTheBabies", () => {
+    it("When passed an empty string, returns an empty string", () => {
+        expect(herdTheBabies("")).to.eql(""); 
+    })
+    it("When passed an already sorted string, returns original string", () => {
+        expect(herdTheBabies("abc")).to.eql("abc"); 
+        expect(herdTheBabies("ABC")).to.eql("ABC"); 
+    })
+    it("When passed a string, returns the string in alphabetical order", () => {
+        expect(herdTheBabies("bac")).to.eql("abc");
+        expect(herdTheBabies("BAC")).to.eql("ABC");
+    })
+    it("When passed an upper and lower case, returns the upper case first", () => {
+        expect(herdTheBabies("aA")).to.eql("Aa"); 
+        expect(herdTheBabies("aBA")).to.eql("AaB");
+        expect(herdTheBabies("bbaBccAC")).to.eql("AaBbbCcc");
+    })
+});
